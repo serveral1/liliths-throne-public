7e97c8bdfd9aae9cbec405182f7eac9cde992c67
serveral1 <hawkblackdown@hotmail.com>
2020-04-06 19:28:28 -0400

    penis lenght value descriptor tweaks

--- src/com/lilithsthrone/game/character/body/valueEnums/PenisLength.java
+++ src/com/lilithsthrone/game/character/body/valueEnums/PenisLength.java
@@ -21,22 +21,22 @@ public enum PenisLength {
 	ONE_TINY("small", 5, 10, PresetColour.GENERIC_SIZE_TWO),
 
 	/**Average size for all regular morphs. (4-7 inches)*/
-	TWO_AVERAGE("average-sized", 10, 20, PresetColour.GENERIC_SIZE_THREE),
+	TWO_AVERAGE("average-sized", 10, 18, PresetColour.GENERIC_SIZE_THREE),
 
 	/**Average size for a wolf morph. (8-11 inches)*/
-	THREE_LARGE("large", 20, 30, PresetColour.GENERIC_SIZE_FOUR),
+	THREE_LARGE("large", 18, 29, PresetColour.GENERIC_SIZE_FOUR),
 
 	/**Average size for a horse morph. (12-15 inches)*/
-	FOUR_HUGE("huge", 30, 40, PresetColour.GENERIC_SIZE_FIVE),
+	FOUR_HUGE("huge", 29, 39, PresetColour.GENERIC_SIZE_FIVE),
 
 	/**Straying into the bounds of "world record for a human". Large for a horse morph. (16-19 inches)*/
-	FIVE_ENORMOUS("enormous", 40, 50, PresetColour.GENERIC_SIZE_SIX),
+	FIVE_ENORMOUS("enormous", 39, 48, PresetColour.GENERIC_SIZE_SIX),
 
 	/**This is just ridiculous... World record for a horse-morph. (20-23 inches)*/
-	SIX_GIGANTIC("gigantic", 50, 60, PresetColour.GENERIC_SIZE_SEVEN),
+	SIX_GIGANTIC("gigantic", 48, 58, PresetColour.GENERIC_SIZE_SEVEN),
 
 	/**And this is for "extreme proportion" content. (24-40 inches)*/
-	SEVEN_STALLION("stallion-sized", 60, 100, PresetColour.GENERIC_SIZE_EIGHT);
+	SEVEN_STALLION("stallion-sized", 58, 100, PresetColour.GENERIC_SIZE_EIGHT);
 
 	private int minimumValue, maximumValue;
 	private String descriptor;
