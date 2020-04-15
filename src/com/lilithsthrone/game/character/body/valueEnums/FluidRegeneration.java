1cbecdeca07b80235c62cec3f54d3895725b386d
serveral1 <hawkblackdown@hotmail.com>
2020-04-15 00:19:25 -0400

    cum and race tweaks

--- src/com/lilithsthrone/game/character/body/valueEnums/FluidRegeneration.java
+++ src/com/lilithsthrone/game/character/body/valueEnums/FluidRegeneration.java
@@ -12,20 +12,20 @@ import com.lilithsthrone.utils.colours.PresetColour;
  */
 public enum FluidRegeneration {
 
-	/** 0-249 ml/day. */
-	ZERO_SLOW("slow", 0, 250, "slowly", PresetColour.GENERIC_SIZE_ONE),
+	/** 0-50 ml/day. */
+	ZERO_SLOW("slow", 0, 100, "slowly", PresetColour.GENERIC_SIZE_ONE),
 	
-	/** 250-749 ml/day. <b>Median value of 500ml/day is roughly the average for breasts producing milk.</b> */
-	ONE_AVERAGE("average", 250, 750, "", PresetColour.GENERIC_SIZE_TWO),
+	/** 50-500ml/day. */
+	ONE_AVERAGE("average", 50, 500, "", PresetColour.GENERIC_SIZE_TWO),
 
-	/** 750-4999 ml/day. Maximum value is roughly 3.5ml per minute. */
-	TWO_FAST("fast", 750, 5000, "quickly", PresetColour.GENERIC_SIZE_THREE),
+	/** 500-2500 ml/day. Maximum value is roughly 0.69ml per minute. */
+	TWO_FAST("fast", 500, 2500, "quickly", PresetColour.GENERIC_SIZE_THREE),
 
-	/** 5000-99,999 ml/day. <b>This median value of ~50,000ml/day is a more realistic average for seminal fluid replenishment, but for gameplay purposes, the default is 10,000.</b> Maximum value is roughly 1.15ml per second. */
-	THREE_RAPID("rapid", 5000, 100_000, "rapidly", PresetColour.GENERIC_SIZE_FOUR),
+	/** 2500-10,000 ml/day. Maximum value is roughly 2.77ml per minute. */
+	THREE_RAPID("rapid", 2500, 10_000, "rapidly", PresetColour.GENERIC_SIZE_FOUR),
 	
-	/** 100,000-500,000 ml/day. Maximum value is roughly 5.8ml per second. */
-	FOUR_VERY_RAPID("very rapid", 100_000, 500_000, "very rapidly", PresetColour.GENERIC_SIZE_FIVE);
+	/** 10,000-50,000 ml/day. Maximum value is roughly 13.88ml per minute. */
+	FOUR_VERY_RAPID("very rapid", 10_000, 50_000, "very rapidly", PresetColour.GENERIC_SIZE_FIVE);
 	
 
 	private int minimumValue;
@@ -34,7 +34,7 @@ public enum FluidRegeneration {
 	private String verb;
 	private Colour colour;
 	
-	public static int CUM_REGEN_DEFAULT = 10_000;
+	public static int CUM_REGEN_DEFAULT = 500;
 
 	private FluidRegeneration(String name, int minimumValue, int maximumValue, String verb, Colour colour) {
 		this.name = name;
